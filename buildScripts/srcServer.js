f84181ac02f6970715be34efe5a85abbd0fe7371
Reuben <reuben@Reubens-MacBook-Pro.local>
2019-12-22 17:08:05 +0100

    need to finish webpack setup

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,12 +1,21 @@
-const express = require('express');
-const path = require('path');
-const open = require('open');
+import express from 'express';
+import { join } from 'path';
+import open from 'open';
+
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath,
+}));
 
 app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, '../src/index.html'));
+    res.sendFile(join(__dirname, '../src/index.html'));
 });
 
 app.listen(PORT, (err) => {
