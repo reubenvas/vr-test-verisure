bf9f4ae71bf8312978c1b2612431e81b5c990b34
Reuben <reuben@Reubens-MBP.lan>
2020-01-02 15:39:05 +0100

    appveyor ci server

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import generateCourseValue from './courseValue';
 
 // debugger; For souremaps to find
 const courseValue = generateCourseValue(1000);
+// eslint-disable-next-line no-console
 console.log(`I would pay ${courseValue} for this awesome course`);
 
 export default generateCourseValue;
