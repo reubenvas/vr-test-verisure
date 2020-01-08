d1a0fa5e4c17a44e8a001067ca47022bf8ed425c
Reuben <reuben@Reubens-MBP.lan>
2020-01-08 21:06:03 +0100

    updated ipv4-adress-generating-function

--- buildScripts/helpers/getIPv4Adress.ts
+++ buildScripts/helpers/getIPv4Adress.ts
@@ -3,9 +3,13 @@ import os from 'os';
 
 export default (): string => {
     const networkInterfaces = os.networkInterfaces();
+    let IPv4Index = -1;
     for (const networkInterface of Object.values(networkInterfaces)) {
         for (const networkInterfaceInfo of networkInterface) {
             if (networkInterfaceInfo.family === 'IPv4') {
+                IPv4Index += 1;
+            }
+            if (IPv4Index === 1) {
                 return networkInterfaceInfo.address;
             }
         }
--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,4 +1,5 @@
 export default () => {
     const inDevelopment = process.env.NODE_ENV === 'development';
+    // const devUrlOrigin = window.location.href.replace(window.location.port, '3001');
     return inDevelopment ? 'http://localhost:3001/' : '/';
 };
