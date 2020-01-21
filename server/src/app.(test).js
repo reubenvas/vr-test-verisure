35d32cd6519941d1818062f590d8b96ebe799b4c
Reuben Vas <reuben.vas@verisure.se>
2020-01-21 14:19:30 +0100

    removed a test that wasn't unit test

--- server/src/app.(test).js
+++ server/src/app.(test).js
@@ -2,9 +2,9 @@ import request from 'supertest';
 import app from './app';
 
 describe('REST API', () => {
-    test('GET to "/"', async (done) => {
+    test('GET to "/"', async (/* done */) => {
         const response = await request(app).get('/');
         expect(response.statusCode).toBe(200);
-        done();
+        /* done(); */
     });
 });
